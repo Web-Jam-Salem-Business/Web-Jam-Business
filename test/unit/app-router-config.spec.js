16b817b08a97bfc77c0981c2ce525b121a3bc8c2
Josh Sherman <joshua.v.sherman@gmail.com>
2017-03-05 19:10:28 -0500

    fixed routes test and updated build script to work on macs

--- test/unit/app-router-config.spec.js
+++ test/unit/app-router-config.spec.js
@@ -1,12 +1,8 @@
-// import {inject} from 'aurelia-framework';
+
 import {AppRouterConfig} from '../../src/app.router.config';
 import {AuthorizeStep} from 'aurelia-auth';
 import {Router} from 'aurelia-router';
-//
-// import {Router} from 'aurelia-router';
-// import AppRouterConfig from "../../src/app.router.config";
-// @inject(Router, AppRouterConfig)
-//import {router} from '../../src/app.router.config';
+
 class RouterStub {
   configure(handler) {
     handler(this);
@@ -17,6 +13,10 @@ class RouterStub {
   }
 
   addPipelineStep(param1, param2) {
+    //do nothing
+  }
+
+  options() {
     //do nothing
   }
 }
@@ -42,23 +42,35 @@ describe('the app.router.config module', () => {
     expect(sut.router.title).toEqual('Web Jam LLC');
   });
 
-  it('should have an About route', () => {
+  it('configures the router to use pushState', () => {
+    expect(sut.router.options.pushState).toBe(true);
+  });
+
+  it('configures the router\'s root to be /', () => {
+    expect(sut.router.options.root).toBe('/');
+  });
+
+  it('should have an Home route', () => {
     expect(sut.router.routes).toContain({ route: ['', 'home'], name: 'home',  moduleId: './home', nav: true, title: 'Home' });
   });
 
-  it('should have a news route', () => {
-    expect(sut.router.routes).toContain({ route: 'samples', name: 'samples', moduleId: './samples', nav: true, title: 'Samples' });
-  });
+  // it('should have a Book Shelf route', () => {
+  //   expect(sut.router.routes).toContain({ route: 'bookshelf', name: 'bookshelf', moduleId: './bookshelf', nav: true, title: 'Book Shelf', settings: 'fa fa-book'});
+  // });
+
+  // it('should have a Create Books route', () => {
+  //   expect(sut.router.routes).toContain({ route: 'createbooks', name: 'createbooks', moduleId: './createBooks', nav: false, title: 'Create Books', settings: 'fa fa-plus'});
+  // });
 
   // it('should have a login route', () => {
-  //   expect(sut.router.routes).toContain({ route: 'login', name: 'login', moduleId: './login', nav: false, title: 'Login' });
+  //   expect(sut.router.routes).toContain({ route: 'login', name: 'login', moduleId: './login', nav: false, title: 'Login', settings: 'fa fa-sign-in'});
   // });
   //
   // it('should have a dashboard route', () => {
-  //   expect(sut.router.routes).toContain({ route: 'dashboard', name: 'dashboard-router', moduleId: './dashboard-router', nav: false, title: 'Dashboard', auth: true });
+  //   expect(sut.router.routes).toContain({ route: 'dashboard', name: 'dashboard-router', moduleId: './dashboard-router', nav: false, title: 'Dashboard', auth: true, settings: 'fa fa-tachometer' });
   // });
-
-  // it('should have a login route', () => {
-  //   expect(sut.router.routes).toContain({ route: 'dashboard', name: 'dashboard', moduleId: './dashboard', nav: false, title: 'Dashboard', auth:true });
+  //
+  // it('should have a release notes route', () => {
+  //   expect(sut.router.routes).toContain({ route: 'releasenotes', name: 'releasenotes', moduleId: './releasenotes', nav: true, title: 'Release Notes', settings: 'fa fa-file-text-o' });
   // });
 });
